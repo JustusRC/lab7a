9418b0ad5df301dac9e0d850edb3590fa5f5e1f2
JustusRC <91702048+JustusRC@users.noreply.github.com>
2021-12-15 14:40:07 -0700

    grades

---
+++ Lab7A/src/lab7a/cs123.java
@@ -0,0 +1,29 @@
+package lab7a;
+
+public class cs123 {
+
+	public static void main(String[] args) {
+		//Array declaration    
+		int [] Grades = {75, 85, 80, 90, 70};
+	
+		
+		    //For loop to put the grades in reverse 
+			for(int i = Grades.length-1; i>=0; i--)
+			{
+				System.out.println(i+" "+Grades[i]);
+			}
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
